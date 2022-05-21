8848a40ab446bc20ab8d1078867aa456620c3457
Derrek <a0926884115@gmail.com>
2022-05-21 17:31:26 +0800

    更新 log4j2 啟用 javascript

--- src/lite/core/listeners/SpringApplicationListener.java
+++ src/lite/core/listeners/SpringApplicationListener.java
@@ -26,7 +26,6 @@ public class SpringApplicationListener implements ApplicationListener<Applicatio
 	@Autowired
 	ScheduledFactory scheduledFactory;
 
-
 	@Override
 	public void onApplicationEvent(ApplicationContextEvent event) {
 		if (event instanceof ContextRefreshedEvent) {
@@ -46,6 +45,10 @@ public class SpringApplicationListener implements ApplicationListener<Applicatio
 
 	private void configLogging() {
 		try {
+
+			// 啟用 JavaScript
+			System.setProperty("log4j2.Script.enableLanguages", "JavaScript");
+
 			// 載入 log4j2 的設定檔
 			loggerContext = Configurator.initialize(null, log4j2ConfigLocation.getFile().getAbsolutePath());
 

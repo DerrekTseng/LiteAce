12c6bb409006394398c76e256f5b3a95488783a8
Derrek <a0926884115@gmail.com>
2022-06-14 23:12:58 +0800

    修正 connection pool 未連線

--- src/lite/core/mybatis/DBProxy.java
+++ src/lite/core/mybatis/DBProxy.java
@@ -1,7 +1,11 @@
 package lite.core.mybatis;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
+import org.apache.commons.dbcp2.PoolableConnection;
+import org.apache.commons.dbcp2.PoolableConnectionFactory;
+import org.apache.commons.pool2.ObjectPool;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,8 +14,18 @@ public class DBProxy {
 	@Resource(name = "mysql_db")
 	MybatisUtil mysql;
 
+	@Resource(name = "mysql_genericObjectPoolBean")
+	ObjectPool<PoolableConnection> genericObjectPool;
+
+	@Resource(name = "mysql_poolableConnectionFactoryBean")
+	PoolableConnectionFactory poolableConnectionFactory;
+
+	@PostConstruct
+	private void postConstruct() {
+		poolableConnectionFactory.setPool(genericObjectPool);
+	}
+
 	public MybatisUtil getMysql() {
 		return mysql;
 	}
-
 }
